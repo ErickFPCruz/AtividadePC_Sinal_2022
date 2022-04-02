485d6157e55281905179ef55bbc8ec00b6bcaa5a
ErickFPCruz <erick.cruz97@hotmail.com>
2022-04-01 21:35:23 -0300

    Ident. de n° inteiros

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+int numero;
+
+Console.Write("Escreva um número qualquer: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+bool numeroNegativo = numero < 0, numeroPositivo = numero > 0, Zero = numero == 0;
+
+if (numeroNegativo)
+{
+    Console.WriteLine("Negativo!");
+}
+if (numeroPositivo)
+{
+    Console.WriteLine("Positivo!");
+}
+if(Zero)
+{
+    Console.WriteLine("Zero!");
+}
